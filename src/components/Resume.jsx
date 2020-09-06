a183f57403d3d707e6fea8266352cd716596030d
Vansikaa <vansibanu@gmail.com>
2020-09-06 18:29:34 +0530

    Resume improvised
    To do: Add spacing, firebase
    
    Co-Authored-By: Rajat Jacob <rajatjacob@gmail.com>

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,67 +2,116 @@ import React from "react"
 import {
 	Grid,
 	TextField,
+	Button,
 	Radio,
 	RadioGroup,
+	Checkbox,
 	FormControl,
 	FormControlLabel,
 	FormLabel,
+	FormGroup,
 } from "@material-ui/core"
 
 export default _ => {
-	const [genderValue, setGenderValue] = React.useState()
-	const handleChange = event => {
-		setGenderValue(event.target.value)
-	}
+	const [name, setName] = React.useState()
+	const [gender, setGender] = React.useState("male")
+	const [dob, setDOB] = React.useState()
+	const [uni, setUni] = React.useState()
+	const [types, setTypes] = React.useState({
+		Job: true,
+		Internship: false,
+		Project: false,
+	})
+
 	return (
 		<div className="Resume">
-			<Grid container direction="column" justify="center" spacing={3}>
-				<TextField label="Name" />
-				<Grid container direction="row" spacing={3}>
-					<Grid item>
-						<TextField label="Email id" />
-					</Grid>
-					<Grid item>
-						<TextField label="Phone" />
-					</Grid>
+			<Grid container direction="column">
+				<Grid item>
+					<TextField
+						label="Name"
+						value={name}
+						onChange={e => {
+							setName(e.target.value)
+						}}
+					/>
+				</Grid>
+				<Grid item>
+					<TextField label="E-mail Address" />
+				</Grid>
+
+				<Grid item>
+					<TextField label="Phone" />
 				</Grid>
 				<FormControl>
 					<FormLabel>Gender</FormLabel>
-					<RadioGroup>
+					<RadioGroup value={gender} onChange={e => setGender(e.target.value)}>
 						<Grid container direction="row">
+							<FormControlLabel value="male" control={<Radio />} label="Male" />
 							<FormControlLabel
-								value="Male"
-								control={<Radio />}
-								label="Male"
-								checked={genderValue === "Male"}
-								onChange={handleChange}
-							/>
-							<FormControlLabel
-								value="Female"
+								value="female"
 								control={<Radio />}
 								label="Female"
-								checked={genderValue === "Female"}
-								onChange={handleChange}
 							/>
 							<FormControlLabel
-								value="Other"
+								value="other"
 								control={<Radio />}
 								label="Other"
-								checked={genderValue === "Other"}
-								onChange={handleChange}
 							/>
 						</Grid>
 					</RadioGroup>
 				</FormControl>
-				<TextField
-					id="DOB"
-					type="date"
-					label="DOB"
-					InputLabelProps={{
-						shrink: true,
-					}}
-				/>
-				<TextField label="University" />
+				<Grid item>
+					<TextField
+						type="date"
+						label="DOB"
+						value={dob}
+						InputLabelProps={{
+							shrink: true,
+						}}
+						onChange={e => {
+							setDOB(e.target.value)
+						}}
+					/>
+				</Grid>
+				<Grid>
+					<TextField
+						label="University"
+						value={uni}
+						onChange={e => {
+							setUni(e.target.value)
+						}}
+					/>
+				</Grid>
+				<FormControl>
+					<FormLabel>Preferences: </FormLabel>
+					<FormGroup
+						onChange={e =>
+							setTypes({ ...types, [e.target.name]: e.target.checked })
+						}
+					>
+						<Grid container direction="row">
+							<FormControlLabel
+								control={<Checkbox name="Job" checked={types.Job} />}
+								label="Job"
+							/>
+							<FormControlLabel
+								control={
+									<Checkbox name="Internship" checked={types.Internship} />
+								}
+								label="Internship"
+							/>
+							<FormControlLabel
+								control={<Checkbox name="Project" checked={types.Project} />}
+								label="Project"
+							/>
+						</Grid>
+					</FormGroup>
+				</FormControl>
+				<Grid item>
+					<Button variant="contained" color="primary">
+						Save
+					</Button>
+				</Grid>
 			</Grid>
 		</div>
 	)
