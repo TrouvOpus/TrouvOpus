42e924cfde8d6ffb33ea9e399cc628e1d2a4e96c
Rajat Jacob <rajatjacob@gmail.com>
2020-09-16 10:32:45 +0530

    Basic Firestore connection to the Resume form

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import {
+	CircularProgress,
 	Grid,
 	TextField,
 	Button,
@@ -11,125 +12,169 @@ import {
 	FormLabel,
 	FormGroup,
 } from "@material-ui/core"
+import { useUser } from "../hooks"
+import FirebaseContext from "../contexts/FirebaseContext"
 
 export default _ => {
+	const { Auth } = React.useContext(FirebaseContext)
+
+	const uid = Auth && Auth.currentUser && Auth.currentUser.uid
+	const { user, updateUser } = useUser(uid, true)
+
 	const [name, setName] = React.useState()
 	const [email, setEmail] = React.useState()
 	const [phone, setPhone] = React.useState()
 	const [gender, setGender] = React.useState("male")
 	const [dob, setDOB] = React.useState()
-	const [types, setTypes] = React.useState({
-		Job: true,
-		Internship: false,
-		Project: false,
+	const [uni, setUni] = React.useState()
+	const [jobPref, setJobPref] = React.useState({
+		job: true,
+		internship: false,
+		project: false,
 	})
+	const [isLoading, setIsLoading] = React.useState(false)
+
+	React.useEffect(() => {
+		setIsLoading(!user)
+		if (user) {
+			setName(user.name)
+			setGender(user.gender)
+			setUni(user.uni)
+			setJobPref(user.jobPref)
+		}
+	}, [user])
+
+	const formData = { name, gender, dob, uni, jobPref }
+
+	function getUpdatedData() {
+		let data = {}
+		Object.keys(formData).forEach(d => {
+			if (formData[d]) data[d] = formData[d]
+		})
+		return data
+	}
+
+	async function save() {
+		try {
+			await updateUser(getUpdatedData())
+		} catch (err) {
+			console.error(err)
+		}
+	}
 
 	return (
 		<div className="Resume">
-			<Grid container direction="column" spacing={1}>
-				<Grid item>
-					<TextField
-						label="Name"
-						value={name}
-						onChange={e => {
-							setName(e.target.value)
-						}}
-					/>
-				</Grid>
-				<Grid container direction="row" spacing={2}>
-					<Grid item>
-						<TextField
-							label="E-mail Address"
-							value={email}
-							type="email"
-							onChange={e => {
-								setEmail(e.target.value)
-							}}
-						/>
+			{isLoading ? (
+				<CircularProgress color="secondary" />
+			) : (
+				<form
+					onSubmit={e => {
+						e.preventDefault()
+						save()
+					}}
+				>
+					<Grid container direction="column">
+						<Grid item>
+							<TextField
+								label="Name"
+								value={name}
+								onChange={e => {
+									setName(e.target.value)
+								}}
+							/>
+						</Grid>
+						<Grid item>
+							<TextField label="E-mail Address" />
+						</Grid>
+
+						<Grid item>
+							<TextField label="Phone" />
+						</Grid>
+						<FormControl>
+							<FormLabel>Gender</FormLabel>
+							<RadioGroup
+								value={gender}
+								onChange={e => setGender(e.target.value)}
+							>
+								<Grid container direction="row">
+									<FormControlLabel
+										value="male"
+										control={<Radio />}
+										label="Male"
+									/>
+									<FormControlLabel
+										value="female"
+										control={<Radio />}
+										label="Female"
+									/>
+									<FormControlLabel
+										value="other"
+										control={<Radio />}
+										label="Other"
+									/>
+								</Grid>
+							</RadioGroup>
+						</FormControl>
+						<Grid item>
+							<TextField
+								type="date"
+								label="DOB"
+								value={dob}
+								InputLabelProps={{
+									shrink: true,
+								}}
+								onChange={e => {
+									setDOB(e.target.value)
+								}}
+							/>
+						</Grid>
+						<Grid>
+							<TextField
+								label="University"
+								value={uni}
+								onChange={e => {
+									setUni(e.target.value)
+								}}
+							/>
+						</Grid>
+						<FormControl>
+							<FormLabel>Preferences: </FormLabel>
+							<FormGroup
+								onChange={e =>
+									setJobPref({ ...jobPref, [e.target.name]: e.target.checked })
+								}
+							>
+								<Grid container direction="row">
+									<FormControlLabel
+										control={<Checkbox name="job" checked={jobPref.job} />}
+										label="Job"
+									/>
+									<FormControlLabel
+										control={
+											<Checkbox
+												name="internship"
+												checked={jobPref.internship}
+											/>
+										}
+										label="Internship"
+									/>
+									<FormControlLabel
+										control={
+											<Checkbox name="project" checked={jobPref.project} />
+										}
+										label="Project"
+									/>
+								</Grid>
+							</FormGroup>
+						</FormControl>
+						<Grid item>
+							<Button type="submit" variant="contained" color="primary">
+								Save
+							</Button>
+						</Grid>
 					</Grid>
-					<Grid item>
-						<TextField
-							label="Phone"
-							value={phone}
-							onChange={e => {
-								setPhone(e.target.value)
-							}}
-						/>
-					</Grid>
-				</Grid>
-				<Grid item>
-					<FormControl>
-						<FormLabel>Gender</FormLabel>
-						<RadioGroup
-							value={gender}
-							onChange={e => setGender(e.target.value)}
-						>
-							<Grid container direction="row" spacing={1}>
-								<FormControlLabel
-									value="male"
-									control={<Radio />}
-									label="Male"
-								/>
-								<FormControlLabel
-									value="female"
-									control={<Radio />}
-									label="Female"
-								/>
-								<FormControlLabel
-									value="other"
-									control={<Radio />}
-									label="Other"
-								/>
-							</Grid>
-						</RadioGroup>
-					</FormControl>
-				</Grid>
-				<Grid item>
-					<TextField
-						type="date"
-						label="DOB"
-						value={dob}
-						InputLabelProps={{
-							shrink: true,
-						}}
-						onChange={e => {
-							setDOB(e.target.value)
-						}}
-					/>
-				</Grid>
-				<Grid item>
-					<FormControl>
-						<FormLabel>Preferences: </FormLabel>
-						<FormGroup
-							onChange={e =>
-								setTypes({ ...types, [e.target.name]: e.target.checked })
-							}
-						>
-							<Grid container direction="row">
-								<FormControlLabel
-									control={<Checkbox name="Job" checked={types.Job} />}
-									label="Job"
-								/>
-								<FormControlLabel
-									control={
-										<Checkbox name="Internship" checked={types.Internship} />
-									}
-									label="Internship"
-								/>
-								<FormControlLabel
-									control={<Checkbox name="Project" checked={types.Project} />}
-									label="Project"
-								/>
-							</Grid>
-						</FormGroup>
-					</FormControl>
-				</Grid>
-				<Grid item>
-					<Button variant="contained" color="primary">
-						Save
-					</Button>
-				</Grid>
-			</Grid>
+				</form>
+			)}
 		</div>
 	)
 }
