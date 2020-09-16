4272278d0b492dd3a8fc17477e17f8ee38818a0f
Vansikaa <vansibanu@gmail.com>
2020-09-16 15:57:11 +0530

    Spacing in Resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -14,9 +14,10 @@ import {
 
 export default _ => {
 	const [name, setName] = React.useState()
+	const [email, setEmail] = React.useState()
+	const [phone, setPhone] = React.useState()
 	const [gender, setGender] = React.useState("male")
 	const [dob, setDOB] = React.useState()
-	const [uni, setUni] = React.useState()
 	const [types, setTypes] = React.useState({
 		Job: true,
 		Internship: false,
@@ -25,7 +26,7 @@ export default _ => {
 
 	return (
 		<div className="Resume">
-			<Grid container direction="column">
+			<Grid container direction="column" spacing={1}>
 				<Grid item>
 					<TextField
 						label="Name"
@@ -35,31 +36,54 @@ export default _ => {
 						}}
 					/>
 				</Grid>
-				<Grid item>
-					<TextField label="E-mail Address" />
+				<Grid container direction="row" spacing={2}>
+					<Grid item>
+						<TextField
+							label="E-mail Address"
+							value={email}
+							type="email"
+							onChange={e => {
+								setEmail(e.target.value)
+							}}
+						/>
+					</Grid>
+					<Grid item>
+						<TextField
+							label="Phone"
+							value={phone}
+							onChange={e => {
+								setPhone(e.target.value)
+							}}
+						/>
+					</Grid>
 				</Grid>
-
 				<Grid item>
-					<TextField label="Phone" />
+					<FormControl>
+						<FormLabel>Gender</FormLabel>
+						<RadioGroup
+							value={gender}
+							onChange={e => setGender(e.target.value)}
+						>
+							<Grid container direction="row" spacing={1}>
+								<FormControlLabel
+									value="male"
+									control={<Radio />}
+									label="Male"
+								/>
+								<FormControlLabel
+									value="female"
+									control={<Radio />}
+									label="Female"
+								/>
+								<FormControlLabel
+									value="other"
+									control={<Radio />}
+									label="Other"
+								/>
+							</Grid>
+						</RadioGroup>
+					</FormControl>
 				</Grid>
-				<FormControl>
-					<FormLabel>Gender</FormLabel>
-					<RadioGroup value={gender} onChange={e => setGender(e.target.value)}>
-						<Grid container direction="row">
-							<FormControlLabel value="male" control={<Radio />} label="Male" />
-							<FormControlLabel
-								value="female"
-								control={<Radio />}
-								label="Female"
-							/>
-							<FormControlLabel
-								value="other"
-								control={<Radio />}
-								label="Other"
-							/>
-						</Grid>
-					</RadioGroup>
-				</FormControl>
 				<Grid item>
 					<TextField
 						type="date"
@@ -73,40 +97,33 @@ export default _ => {
 						}}
 					/>
 				</Grid>
-				<Grid>
-					<TextField
-						label="University"
-						value={uni}
-						onChange={e => {
-							setUni(e.target.value)
-						}}
-					/>
+				<Grid item>
+					<FormControl>
+						<FormLabel>Preferences: </FormLabel>
+						<FormGroup
+							onChange={e =>
+								setTypes({ ...types, [e.target.name]: e.target.checked })
+							}
+						>
+							<Grid container direction="row">
+								<FormControlLabel
+									control={<Checkbox name="Job" checked={types.Job} />}
+									label="Job"
+								/>
+								<FormControlLabel
+									control={
+										<Checkbox name="Internship" checked={types.Internship} />
+									}
+									label="Internship"
+								/>
+								<FormControlLabel
+									control={<Checkbox name="Project" checked={types.Project} />}
+									label="Project"
+								/>
+							</Grid>
+						</FormGroup>
+					</FormControl>
 				</Grid>
-				<FormControl>
-					<FormLabel>Preferences: </FormLabel>
-					<FormGroup
-						onChange={e =>
-							setTypes({ ...types, [e.target.name]: e.target.checked })
-						}
-					>
-						<Grid container direction="row">
-							<FormControlLabel
-								control={<Checkbox name="Job" checked={types.Job} />}
-								label="Job"
-							/>
-							<FormControlLabel
-								control={
-									<Checkbox name="Internship" checked={types.Internship} />
-								}
-								label="Internship"
-							/>
-							<FormControlLabel
-								control={<Checkbox name="Project" checked={types.Project} />}
-								label="Project"
-							/>
-						</Grid>
-					</FormGroup>
-				</FormControl>
 				<Grid item>
 					<Button variant="contained" color="primary">
 						Save
