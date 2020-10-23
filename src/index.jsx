141114c340b5ecae16954d36ba2e9bcd3c1b3143
Renil Justin <reniljustin66@gmail.com>
2020-10-23 20:48:30 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,11 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 import { BrowserRouter as Router } from "react-router-dom"
-import chat from "./components/chat"
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Router>
 			<App />
-		</Router>
-		<Router>
-			<chat />
 		</Router>
 	</React.StrictMode>,
 	document.getElementById("root")
